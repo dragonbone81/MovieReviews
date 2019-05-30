b0562e7030d568efcf218f3c69bf3cc12bdd1f26
dragonbone81 <cvernikoff@gmail.com>
2019-05-30 10:46:29 -0700

    some descriptions and ui stuff

--- client/src/Main/MoviePage.js
+++ client/src/Main/MoviePage.js
@@ -14,7 +14,11 @@ class MoviePage extends Component {
             document.title = movie_id;
             const data = await this.props.store.getMovieInfo(movie_id);
             const OMDB_data = await this.props.store.getMovieInfoOMDB(data.imdb_id);
-            data.ratings = this.props.store.getRatings(OMDB_data);
+            if (OMDB_data.Response === "False") {
+                data.ratings = [];
+            } else {
+                data.ratings = this.props.store.getRatings(OMDB_data);
+            }
             this.setState({imageData: data});
             document.title = data.title;
             console.log(data);
@@ -27,7 +31,11 @@ class MoviePage extends Component {
         document.title = movie_id;
         const data = await this.props.store.getMovieInfo(movie_id);
         const OMDB_data = await this.props.store.getMovieInfoOMDB(data.imdb_id);
-        data.ratings = this.props.store.getRatings(OMDB_data);
+        if (OMDB_data.Response === "False") {
+            data.ratings = [];
+        } else {
+            data.ratings = this.props.store.getRatings(OMDB_data);
+        }
         this.setState({imageData: data});
         document.title = data.title;
         console.log(data);
@@ -41,11 +49,12 @@ class MoviePage extends Component {
                     <div
                         style={{backgroundImage: `url(${this.props.store.getImageURL(this.state.imageData.backdrop_path)})`}}
                         className="movie-backdrop"/>
-                    <div className="mr-1 ml-1 movie-content d-flex flex-row justify-content-around">
+                    <div className="mr-1 ml-1 movie-content d-flex flex-row justify-content-center">
                         <div className="movie-poster d-flex flex-column mr-2">
-                            <img width={250} src={this.props.store.getImageURL(this.state.imageData.poster_path)}
+                            <img width={250}
+                                 src={this.state.imageData.poster_path ? this.props.store.getImageURL(this.state.imageData.poster_path) : "https://i.imgur.com/IiA2iLz.png"}
                                  className="img-thumbnail" alt="Movie poster"/>
-                            <div className="d-flex flex-row justify-content-between ratings mt-1">
+                            <div className="d-flex flex-row justify-content-between ratings mt-2">
                                 <div className="d-flex flex-row mr-1">
                                     <img className="mr-1" height={25}
                                          src="//i.imgur.com/2Sle5QI.png"/>
@@ -69,13 +78,19 @@ class MoviePage extends Component {
                         {/*<div className="movie-poster"*/}
                         {/*style={{backgroundImage: `url(${this.props.store.getImageURL(this.state.imageData.poster_path)})`}}/>*/}
                         <div className="movie-info d-flex flex-column">
-                            <div className="movie-title">{this.state.imageData.title}</div>
-                            <div className="movie-director-year">
-                                Directed
-                                by {this.props.store.getDirectors(this.state.imageData.credits).map((director, i, arr) => {
-                                return (<span key={director.id}>{director.name}{arr.length - 1 !== i && (
-                                    <span>, </span>)}</span>)
-                            })} | <span>{this.state.imageData.release_date.substring(0, 4)}</span>
+                            <div className="d-flex flex-row align-items-center">
+                                <div className="movie-title">{this.state.imageData.title}</div>
+                                <div className="movie-director-year">
+                                    Directed
+                                    by {this.props.store.getDirectors(this.state.imageData.credits).map((director, i, arr) => {
+                                    return (<span key={director.id}>{director.name}{arr.length - 1 !== i && (
+                                        <span>, </span>)}</span>)
+                                })} | <span>{this.state.imageData.release_date.substring(0, 4)}</span>
+                                </div>
+                            </div>
+                            <div className="d-flex flex-column align-items-start">
+                                <div className="tag-line">{this.state.imageData.tagline}</div>
+                                <div className="move-description-detail">{this.state.imageData.overview}</div>
                             </div>
                         </div>
                     </div>
