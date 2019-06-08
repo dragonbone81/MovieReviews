7baeb9008368cd7c08cc790bfff794621fc85e82
Christian <cvernikoff@gmail.com>
2019-06-08 14:05:10 -0700

    cast crew expansion

--- client/src/Misc/PersonCast.js
+++ client/src/Misc/PersonCast.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
 import PersonCastPill from '../Misc/PersonCastPill';
 
 const PersonCast = ({credits, getImageURL, size, created_by}) => {
     const [selectedPill, setSelectedPill] = useState("cast");
+    const [expandedCast, setExpandedCast] = useState(false);
+    const [expandedCrew, setExpandedCrew] = useState(false);
     return (
         <div className="movie-people">
             <div className="people-div-options border-bottom">
@@ -14,32 +15,37 @@ const PersonCast = ({credits, getImageURL, size, created_by}) => {
             </div>
             {selectedPill === "cast" && (
                 <div className="movie-cast d-flex flex-row flex-wrap">
-                    {credits.cast.map(person => {
+                    {credits.cast.slice(0, expandedCast ? credits.cast.length + 1 : 20).map(person => {
                         return (
                             <PersonCastPill size={size}
+                                            key={`${person.id} ${person.job}`}
                                             id={person.id}
                                             getImageURL={getImageURL}
                                             url={person.profile_path}
                                             character={person.character} actor={person.name}/>
                         )
                     })}
+                    <div onClick={() => setExpandedCast(!expandedCast)} className="person-cast-pill-div"><span
+                        className="person-cast-pill">{expandedCast ? "Less..." : "More..."}</span></div>
                 </div>
             )}
             {selectedPill === "crew" && (
                 <div className="movie-cast d-flex flex-row flex-wrap">
-                    {credits.crew.length>0?credits.crew.map(person => {
-                        return (
-                            <PersonCastPill size={size}
-                                            id={person.id}
-                                            getImageURL={getImageURL}
-                                            url={person.profile_path}
-                                            character={person.job} actor={person.name}/>
-                        )
-                    })
-                        :
-                        created_by.map(person => {
+                    {credits.crew.length > 0 ? credits.crew.slice(0, expandedCrew ? credits.cast.length + 1 : 20).map(person => {
                             return (
                                 <PersonCastPill size={size}
+                                                key={`${person.id} ${person.job}`}
+                                                id={person.id}
+                                                getImageURL={getImageURL}
+                                                url={person.profile_path}
+                                                character={person.job} actor={person.name}/>
+                            )
+                        })
+                        :
+                        created_by.slice(0, expandedCrew ? credits.cast.length + 1 : 20).map(person => {
+                            return (
+                                <PersonCastPill size={size}
+                                                key={`${person.id} ${person.job}`}
                                                 id={person.id}
                                                 getImageURL={getImageURL}
                                                 url={person.profile_path}
@@ -47,6 +53,8 @@ const PersonCast = ({credits, getImageURL, size, created_by}) => {
                             )
                         })
                     }
+                    <div onClick={() => setExpandedCrew(!expandedCrew)} className="person-cast-pill-div"><span
+                        className="person-cast-pill">{expandedCrew ? "Less..." : "More..."}</span></div>
                 </div>
             )}
         </div>
