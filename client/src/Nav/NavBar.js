0daba72f766d8e3b622daebd1c07a471a8b22806
Christian <cvernikoff@gmail.com>
2019-06-09 09:43:56 -0700

    some small stuff

--- client/src/Nav/NavBar.js
+++ client/src/Nav/NavBar.js
@@ -5,21 +5,24 @@ import MovieSearch from './MovieSearch';
 
 class NavBar extends Component {
     render() {
+        console.log(this.props)
         return (
             <div
-                className={`trans-nav d-flex flex-row align-items-center`}>
+                className={`${this.props.location.pathname.startsWith("/movie/") ? "trans-nav-movie-view" : "trans-nav"} d-flex flex-row align-items-center`}>
                 <div
                     className="ml-auto mr-auto d-flex flex-row align-items-center nav-bar-content justify-content-between">
                     <span>C-Views</span>
                     <div className="d-flex flex-row ml-auto align-items-center">
                         {this.props.store.userLoggedIn && (
-                            <span
-                                onClick={() => this.props.history.push(`/user/movies/${this.props.store.user.username}`)}
-                                className="username">{this.props.store.user.username}</span>
+                            <Link to={`/user/movies/${this.props.store.user.username}`}
+                                  style={{textDecoration: 'none', color: 'inherit'}}>
+                                <span className="username">{this.props.store.user.username}</span>
+                            </Link>
                         )}
 
                         {!this.props.store.userLoggedIn && (
-                            <Link className="" to="/login">Login</Link>
+                            <Link className="login-button" to="/login"
+                                  style={{textDecoration: 'none'}}>Login</Link>
                         )}
                         {/*{this.props.store.userLoggedIn && (*/}
                         {/*<Link className="" to="/logout">Logout</Link>*/}
