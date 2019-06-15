eef861944041c83c99d9cb027439f19280071195
Christian <cvernikoff@gmail.com>
2019-06-15 10:52:25 -0700

    movie-search res mobile

--- client/src/Misc/MovieSearchResult.js
+++ client/src/Misc/MovieSearchResult.js
@@ -5,13 +5,12 @@ import ImageWithLoading from '../Misc/ImageWithLoading';
 const MovieSearchResult = ({entity, poster_size, getImageURL}) => {
     if (entity.media_type === "movie") {
         const movie = entity;
+        const overview = movie.overview.length > 250 ? `${movie.overview.slice(0, 250)}...` : movie.overview;
         return (
             <div className="d-flex flex-row movie-results-list-item">
-                <div className="d-flex flex-row align-items-center">
-                    <ImageWithLoading type={"movie"} makeLink={true} movie_id={movie.id} width={100}
-                                      imgStyle="movie-poster-image-list"
-                                      src={getImageURL(movie.poster_path, poster_size)}/>
-                </div>
+                <ImageWithLoading type={"movie"} makeLink={true} movie_id={movie.id} width={100}
+                                  imgStyle="movie-poster-image-list"
+                                  src={getImageURL(movie.poster_path, poster_size)}/>
                 <div className="d-flex flex-column movie-list-content">
                     <div className="d-flex flex-row">
                                         <span className="movie-title-list"><Link
@@ -21,7 +20,7 @@ const MovieSearchResult = ({entity, poster_size, getImageURL}) => {
                         <span
                             className="movie-release-year-list">{movie.release_date.substring(0, 4)}</span>
                     </div>
-                    <div className="movie-list-description">{movie.overview}</div>
+                    <div className="movie-list-description">{overview}</div>
                 </div>
 
             </div>
@@ -29,13 +28,12 @@ const MovieSearchResult = ({entity, poster_size, getImageURL}) => {
     }
     if (entity.media_type === "tv") {
         const tv_show = entity;
+        const overview = tv_show.overview.length > 250 ? `${tv_show.overview.slice(0, 250)}...` : tv_show.overview;
         return (
             <div className="d-flex flex-row movie-results-list-item">
-                <div className="d-flex flex-row align-items-center">
-                    <ImageWithLoading type={"tv"} makeLink={true} movie_id={tv_show.id} width={100}
-                                      imgStyle="movie-poster-image-list"
-                                      src={getImageURL(tv_show.poster_path, poster_size)}/>
-                </div>
+                <ImageWithLoading type={"tv"} makeLink={true} movie_id={tv_show.id} width={100}
+                                  imgStyle="movie-poster-image-list"
+                                  src={getImageURL(tv_show.poster_path, poster_size)}/>
                 <div className="d-flex flex-column movie-list-content">
                     <div className="d-flex flex-row">
                         <div className="movie-title-list">
@@ -48,7 +46,7 @@ const MovieSearchResult = ({entity, poster_size, getImageURL}) => {
                         <span
                             className="movie-release-year-list">{tv_show.first_air_date.substring(0, 4)}</span>
                     </div>
-                    <div className="movie-list-description">{tv_show.overview}</div>
+                    <div className="movie-list-description">{overview}</div>
                 </div>
 
             </div>
